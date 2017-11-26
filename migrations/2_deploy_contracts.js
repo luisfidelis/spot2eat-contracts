7a7d204e0279493e16671be56852886bdcb8d476
Luis Philipe Fidelis <luizsale10@gmail.com>
2017-11-26 19:30:28 -0200

    deploy Spot2Eat

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,9 @@
+const SpotLibrary = artifacts.require("./spot/SpotLibrary.sol");
+const Spot2Eat = artifacts.require("./Spot2Eat.sol"); 
+
 module.exports = function(deployer) {
+  deployer.deploy(SpotLibrary).then(() => {
+    return deployer.deploy(Spot2Eat, SpotLibrary.address);
+  });
 
 };
