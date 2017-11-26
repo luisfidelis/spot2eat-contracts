4894014078d88b15fec8322ac97139ab399c5b65
Luis Philipe Fidelis <luizsale10@gmail.com>
2017-11-26 19:05:51 -0200

    add spot

--- contracts/Spot2Eat.sol
+++ contracts/Spot2Eat.sol
@@ -8,7 +8,17 @@ contract Spot2Eat {
 
     mapping(address => Spot[]) public spots;
 
+    event Spots(address _owner, string _description);
+
     function Spot2Eat() {
         owner = msg.sender;
+    }
+
+    function addSpot(string _description) {
+        Spot newSpot = new Spot(_description);
+        Spot[] userSpots = spots[msg.sender];
+        userSpots.push(newSpot);        
+        spots[msg.sender] = userSpots;
+        Spots(msg.sender, _description);
     }
 }
